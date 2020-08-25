288bf659afca63f918f8fca40aecdd98ac4afc1f
Gage Gabaldon <ggmangabaldon@gmail.com>
2020-08-24 18:00:40 -0700

    changed stuff again and added some code i have been working on.

--- Projects/Project 1/ppmMain.c
+++ Projects/Project 1/ppmMain.c
@@ -1,46 +1,55 @@
 int main(int argc, char const *argv[]) {
 
    // initialize varibles
+   unsigned int *pixmap;
+
+   // read file save to pixmap
+      // function: readFile
+   pixmap = readFile(argv[2]);
+
+   // check for what to convert to P3 or P6
 
 
-   // need a check for error if not 8 bit per channel
+   // convert file to P3
+      // function: writeToP3()
 
-   // check if command line is right
-
-     // check if arg[1] is 3 or 6 else return error
-
-     // check if file != NULL save to fileptr
-        // function: fopen
-
-
-   // check what conversion we are doing
-
-      // if argv == 3 then convert to ppm 3
-
-         // read file save to buffer
-            // function:
-
-
-         // convert file to P3
-            // function: convertToP3()
-
-         // write file to output.ppm
-            // function: writeToFile();
-
-   // end ppm3
-
-   // if argv == 6 then convert to ppm 6
-
-      // read file save to buffer
-         // function:
-
-      // convert file to P3
-         // function: convertToP3()
-
-      // write file to output.ppm
-         // function: writeToFile();
-
-   // end ppm6
+   // convert file to P6
+      // fuction: writeToP6()
 
    return 0;
+}
+
+unsigned int readFile(File *filePtr, int number, char *string){
+
+   // determine if it is p6 or p3
+   if(argv[number] == 6)
+   {
+     // intialize varibles
+     const char READ_BYTE_FLAG[] = "rb"
+
+      filePtr = fopen(string, READ_BYTE_FLAG);
+
+      // check if file is found
+      if(filePtr != NULL)
+      {
+
+      }
+      else
+      {
+        printf("File Not Found: \n");
+      }
+
+   }
+
+   if(argv[number] == 3)
+   {
+
+      pixmap = (unsigned int *)malloc(width * height * sizeof(int));
+
+   }
+
+   else
+   {
+      printf("INCORRECT PPM NUMBER ARGUMENT: \n");
+   }
 }
